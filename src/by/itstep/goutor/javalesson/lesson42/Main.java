bd73d59e5b7623d38ef8315f9dfa2cba0b354ee0
nikishkaa <nikita.goutor@gmail.com>
2024-04-18 21:02:27 +0300

    Add Logger in factorial method.

--- src/by/itstep/goutor/javalesson/lesson42/Main.java
+++ src/by/itstep/goutor/javalesson/lesson42/Main.java
@@ -4,6 +4,8 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
+import java.util.Scanner;
+
 public class Main {
     private static final Logger LOGGER;
 
@@ -13,14 +15,35 @@ public class Main {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        LOGGER.setLevel(Level.ERROR);
+        LOGGER.setLevel(Level.INFO);
 
-        LOGGER.trace("call trace method...");
-        LOGGER.debug("call debug method...");
-        LOGGER.info("call info method...");
-        LOGGER.warn("call warn method...");
-        LOGGER.error("call error method...");
-        LOGGER.fatal("call fatal method...");
+//        LOGGER.trace("call trace method...");
+//        LOGGER.debug("call debug method...");
+//        LOGGER.info("call info method...");
+//        LOGGER.warn("call warn method...");
+//        LOGGER.error("call error method...");
+//        LOGGER.fatal("call fatal method...");
 
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        long res = factor(num);
+        String s = String.format("%d! = %d", num, res);
+
+        LOGGER.info(s);
+
+    }
+
+    public static long factor(int number) {
+        LOGGER.debug("Input number is " + number);
+        long res = 1;
+        LOGGER.debug("Start: result = " + res);
+
+        for (int i = 0; i <= number; i++) {
+            res *= i;
+            LOGGER.trace(String.format("i = %d, result = %d", i, res));
+        }
+        LOGGER.debug("return number is " + res);
+        return res;
     }
 }
